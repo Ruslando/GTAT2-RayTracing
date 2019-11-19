00433426d3cfa4ee61733d875c6e6ba3697e5767
thuemmels <thummel.stephan@yahoo.de>
2019-11-20 00:47:03 +0100

    Fixes bug with white ring of death.

--- src/main/OutputController.java
+++ src/main/OutputController.java
@@ -36,8 +36,8 @@ public class OutputController {
         Camera camera = new Camera(new Vector3(0,0,-10), new Vector3(0,0,0));
         Material material = new Material(new Vector3(0,1,0), 0.2, 0.9);
 
-        Shape sphere = new Sphere(2, new Vector3(0,0,0) , material);
-        //Shape sphere = new QuadricSphere(material);
+        //Shape sphere = new Sphere(2, new Vector3(0,0,0) , material);
+        Shape sphere = new QuadricSphere(material);
 
         /*Quadric zylinder = new Zylinder(1);
         Quadric zylinder2 = new CustomQuadric(1,0,1,0,0,0,0,0,0,Math.sqrt(1) * -1, material);
@@ -53,8 +53,8 @@ public class OutputController {
         //scene.addShape(new Sphere(2, new Vector3(0,0,0), new Material(new Vector3(0,1,0))));
         //scene.addShape(new Sphere(4, new Vector3(0,0,4), new Material(new Vector3(0,1,0))));
         //scene.addShape(new Sphere(1, new Vector3(-3,-3,-3), new Material(new Vector3(0,1,0))));
-        //scene.addShape(sphere);
         scene.addShape(sphere);
+        //scene.addShape(zylinder);
         scene.addLight(new Light(new Vector3(0,100,-100), 1, new Vector3(255,255,255), false));
         // scene.addLight(new Light(new Vector3(0,10,-10), 1, new Vector3(255,255,255), false));
 
--- src/main/shapes/Zylinder.java
+++ src/main/shapes/Zylinder.java
@@ -6,6 +6,6 @@ import main.util.Vector3;
 public class Zylinder extends Quadric {
 
     public Zylinder(int radius){
-        super(0,1,1,0,0,0,0,0,0,Math.sqrt(radius) * -1, new Material(new Vector3(1,  0, 0), 0.5, 1));
+        super(0,1,1,0,0,0,0,0,0,Math.sqrt(radius) * -1, new Material(new Vector3(1,  0, 0), 0.1, 0.5));
     }
 }
